88c18a8a8e2935444ffcbee85d6276f8f04f4a2d
Clement 'Hyronos' Soler <47113650+clement-soler1@users.noreply.github.com>
2022-05-30 00:36:06 +0200

    edit cluster and user querying

--- lib/flutter/model/user.dart
+++ lib/flutter/model/user.dart
@@ -1,11 +1,17 @@
 class User {
   final String imagePath;
+  final String firstname;
   final String name;
   final String email;
+  final String password;
+  final String phonenumber;
 
   const User({
     required this.imagePath,
     required this.name,
-    required this.email
+    required this.email,
+    this.firstname = "firstname",
+    this.password = "pwd",
+    this.phonenumber = "00"
   });
 }
---
+++ lib/flutter/provider/user_provider.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:collection'; // nouveaux types de listes comme UnmodifiableListView
+import 'dart:convert'; // pour decoder la r ́eponse http
+//import of model
+import 'package:rando_go/flutter/model/user.dart';
+
+class UserProvider with ChangeNotifier {
+  final String host = 'http://localhost:80';
+
+  // Variable priv ́ee pour qu'elle ne puisse pas ^etre modifi ́ee par
+  // les widgets qui l'utilisent
+  List _users = [
+  ];
+
+  // Getter pour l'acc`es en lecture
+  // Pas de modificiation possible gr^ace au type UnmodifiableListView
+  UnmodifiableListView get users => UnmodifiableListView(_users);
+
+  // M ́ethode pour aller chercher des donn ́ees dans la base de donn ́ees
+  // via le serveur en local
+  void fetchData() async {
+    try {
+      http.Response response = await http.get(Uri.parse('$host/api/profiles'));
+      if (response.statusCode == 200) {
+        /*_profiles = (json.decode(response.body) as List)
+            .map((profileJson) => Profile.fromJson(profileJson))
+            .toList();*/
+        _users = [];
+        var tab = json.decode(response.body) as List;
+        for (var obj in tab) {
+          //_users.add(Profile(obj['firstname'],obj['name'],obj['photo'],obj['phonenumber'],obj['birthdate']));
+          _users.add(User(imagePath: obj["imagePath"], name: obj["name"], email: obj["email"],firstname: obj["firstname"],password: obj["password"],phonenumber: obj["phonenumber"]));
+        }
+        notifyListeners();
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
