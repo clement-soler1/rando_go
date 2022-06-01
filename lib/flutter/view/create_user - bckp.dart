eb6ea9a75b2dc23a603022bf0e6509b725ac03e5
Clement 'Hyronos' Soler <47113650+clement-soler1@users.noreply.github.com>
2022-06-01 22:42:50 +0200

    restoration

---
+++ lib/flutter/view/create_user - bckp.dart
@@ -0,0 +1,216 @@
+/*import 'package:flutter/material.dart';
+import 'package:rando_go/flutter/cards/all_cards.dart';
+import 'package:rando_go/flutter/model/user.dart';
+import 'package:rando_go/flutter/widget/profile_widget.dart';
+import 'package:rando_go/flutter/provider/user_provider.dart';
+import 'package:rando_go/flutter/utils/Security.dart';
+
+class CreateUser extends StatelessWidget {
+  static String routeName = '/create_user';
+
+  static TextEditingController prenom_controller = new TextEditingController();
+  static TextEditingController nom_controller = new TextEditingController();
+  static TextEditingController email_controller = new TextEditingController();
+  static TextEditingController pwd_controller = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: const Color(0xFF009143),
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Creer un compte", style: TextStyle(color: Colors.white,fontSize: 35)),
+                Padding(padding: EdgeInsets.all(30)),
+                ProfileWidget(imagePath: 'images/profile.png', isEdit: true, onClicked: () {},),
+                Padding(padding: EdgeInsets.all(30)),
+                UserForm()
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> handleUserCreation(context,fname,name,pwd,email) async {
+
+    String crypted = Security.encrypt(pwd);
+
+    User newUser = User(imagePath: "", name: name, email: email,password: crypted,firstname: fname,phonenumber: "");
+
+    var provider = UserProvider();
+    provider.addUser(newUser);
+
+    print("user added !");
+
+    Navigator.pushNamed(context,"/user_created");
+  }
+}
+
+class UserForm extends StatefulWidget {
+  const UserForm({key});
+
+  @override
+  UserFormState createState() {
+    return UserFormState();
+  }
+}
+
+class UserFormState extends State<UserForm> {
+  final _formKey = GlobalKey<FormState>();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 400.0,
+            child: Row(
+              children: [
+                Text("Prénom : ",style: TextStyle(color: Colors.white,fontSize: 25),),
+                new Flexible(
+                  child: new TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Prénom',
+                      border: OutlineInputBorder(),
+                      filled: true,
+                      fillColor: Colors.white,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                      )
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Ce champ est obligatoire';
+                      }
+                      return null;
+                    },
+                  ),
+                )
+
+              ],
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(10)),
+          Container(
+            width: 400.0,
+            child: Row(
+              children: [
+                Text("Nom : ",style: TextStyle(color: Colors.white,fontSize: 25),),
+                new Flexible(
+                  child: new TextFormField(
+                    decoration: InputDecoration(
+                        hintText: 'Nom',
+                        border: OutlineInputBorder(),
+                        filled: true,
+                        fillColor: Colors.white,
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                        )
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Ce champ est obligatoire';
+                      }
+                      return null;
+                    },
+                  ),
+                )
+
+              ],
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(10)),
+          Container(
+            width: 400.0,
+            child: Row(
+              children: [
+                Text("Mot de passe : ",style: TextStyle(color: Colors.white,fontSize: 25),),
+                new Flexible(
+                  child: new TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: 'Mot de passe',
+                        border: OutlineInputBorder(),
+                        filled: true,
+                        fillColor: Colors.white,
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                        )
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Ce champ est obligatoire';
+                      }
+                      return null;
+                    },
+                  ),
+                )
+
+              ],
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(10)),
+          Container(
+            width: 400.0,
+            child: Row(
+              children: [
+                Text("Email : ",style: TextStyle(color: Colors.white,fontSize: 25),),
+                new Flexible(
+                  child: new TextFormField(
+                    decoration: InputDecoration(
+                        hintText: 'Email',
+                        border: OutlineInputBorder(),
+                        filled: true,
+                        fillColor: Colors.white,
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                        )
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Ce champ est obligatoire';
+                      }
+                      return null;
+                    },
+                  ),
+                )
+
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                onPrimary: Colors.black,
+                primary: Colors.white
+              ),
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  //_formKey.currentState.save();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Sauvegarde en cours')),
+                  );
+
+                  handleUserCreation()
+                  //Navigator.pushNamed(context,"/app");
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}*/
--- lib/flutter/view/create_user.dart
+++ lib/flutter/view/create_user.dart
@@ -27,8 +27,39 @@ class CreateUser extends StatelessWidget {
                 Text("Creer un compte", style: TextStyle(color: Colors.white,fontSize: 35)),
                 Padding(padding: EdgeInsets.all(30)),
                 ProfileWidget(imagePath: 'images/profile.png', isEdit: true, onClicked: () {},),
-                Padding(padding: EdgeInsets.all(30)),
-                UserForm()
+                //Image(image: AssetImage('images/profile.png')),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Prénom : ",style: TextStyle(color: Colors.white,fontSize: 25),),
+                    RGTextInput(label:"Prénom", width: 200, obscureText: false, controller: prenom_controller,),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Nom : ",style: TextStyle(color: Colors.white,fontSize: 25),),
+                    RGTextInput(label:"Nom", width: 200, obscureText: false,controller: nom_controller,),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Mot de passe : ",style: TextStyle(color: Colors.white,fontSize: 25),),
+                    RGTextInput(label:"Mot de passe", width: 200, obscureText: true, controller: pwd_controller,),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Email : ",style: TextStyle(color: Colors.white,fontSize: 25),),
+                    RGTextInput(label:"Email", width: 200, obscureText: false,controller: email_controller,),
+                  ],
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+                RGButton(label: "Creer mon compte", width: 350, height: 50,onPressed: () => {
+                  handleUserCreation(context,prenom_controller.text, nom_controller.text, pwd_controller.text, email_controller.text)
+                },)
               ],
             ),
           ),
@@ -49,167 +80,5 @@ class CreateUser extends StatelessWidget {
     print("user added !");
 
     Navigator.pushNamed(context,"/user_created");
-  }
-}
-
-class UserForm extends StatefulWidget {
-  const UserForm({key});
-
-  @override
-  UserFormState createState() {
-    return UserFormState();
-  }
-}
-
-class UserFormState extends State<UserForm> {
-  final _formKey = GlobalKey<FormState>();
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            width: 400.0,
-            child: Row(
-              children: [
-                Text("Prénom : ",style: TextStyle(color: Colors.white,fontSize: 25),),
-                new Flexible(
-                  child: new TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'Prénom',
-                      border: OutlineInputBorder(),
-                      filled: true,
-                      fillColor: Colors.white,
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                      )
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Ce champ est obligatoire';
-                      }
-                      return null;
-                    },
-                  ),
-                )
-
-              ],
-            ),
-          ),
-          Padding(padding: EdgeInsets.all(10)),
-          Container(
-            width: 400.0,
-            child: Row(
-              children: [
-                Text("Nom : ",style: TextStyle(color: Colors.white,fontSize: 25),),
-                new Flexible(
-                  child: new TextFormField(
-                    decoration: InputDecoration(
-                        hintText: 'Nom',
-                        border: OutlineInputBorder(),
-                        filled: true,
-                        fillColor: Colors.white,
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                        )
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Ce champ est obligatoire';
-                      }
-                      return null;
-                    },
-                  ),
-                )
-
-              ],
-            ),
-          ),
-          Padding(padding: EdgeInsets.all(10)),
-          Container(
-            width: 400.0,
-            child: Row(
-              children: [
-                Text("Mot de passe : ",style: TextStyle(color: Colors.white,fontSize: 25),),
-                new Flexible(
-                  child: new TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        hintText: 'Mot de passe',
-                        border: OutlineInputBorder(),
-                        filled: true,
-                        fillColor: Colors.white,
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                        )
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Ce champ est obligatoire';
-                      }
-                      return null;
-                    },
-                  ),
-                )
-
-              ],
-            ),
-          ),
-          Padding(padding: EdgeInsets.all(10)),
-          Container(
-            width: 400.0,
-            child: Row(
-              children: [
-                Text("Email : ",style: TextStyle(color: Colors.white,fontSize: 25),),
-                new Flexible(
-                  child: new TextFormField(
-                    decoration: InputDecoration(
-                        hintText: 'Email',
-                        border: OutlineInputBorder(),
-                        filled: true,
-                        fillColor: Colors.white,
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                        )
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Ce champ est obligatoire';
-                      }
-                      return null;
-                    },
-                  ),
-                )
-
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                onPrimary: Colors.black,
-                primary: Colors.white
-              ),
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  //_formKey.currentState.save();
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Sauvegarde en cours')),
-                  );
-
-                  Navigator.pushNamed(context,"/app");
-                }
-              },
-              child: const Text('Submit'),
-            ),
-          ),
-        ],
-      ),
-    );
   }
 }
